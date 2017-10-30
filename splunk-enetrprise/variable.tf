4ff9069d37916fe07b51b4c846d3a280058d7526
sushmithan <sushmithan@sysgain.com>
2017-10-30 18:39:11 +0530

    Update variable.tf

--- splunk-enetrprise/variable.tf
+++ splunk-enetrprise/variable.tf
@@ -2,18 +2,22 @@ variable "tenancy_ocid" {
     type    = "string"
     default = "ocid1.tenancy.oc1..aaaaaaaaa4jvtf7x54iv54vz77ivqui2nlb7ordznpupnvqbaktl6vkwfhga"
 }
+
 variable "user_ocid" {
     type    = "string"
     default = "ocid1.user.oc1..aaaaaaaat5pk2vhfib7bnf6n3bqsw2mjlieisfv4yfjtw5xvrkwpvvhkiloq"
 }
+
 variable "fingerprint" {
     type    = "string"
     default = "71:ea:80:93:a1:92:f8:32:85:05:66:b2:9e:fe:74:40"
 }
+
 variable "private_key_path" {
     type    = "string"
     default = "c:/Users/sushmithan/AppData/Roaming/oci_api_keys/obmcpvt.pem"
 }
+
 variable "region" {
     type    = "string"
     default = "us-ashburn-1"
@@ -21,12 +25,15 @@ variable "region" {
 variable "VCN-CIDR" {
   default = "10.0.0.0/16"
 }
+
 variable "VCN-DisplayName" {
   default = "splunkvcn"
 }
+
 variable "VCN-DNSLabel" {
   default = "splunkvcn"
 }
+
 variable "COMP-ID" {
   default = "ocid1.compartment.oc1..aaaaaaaajgefvmehc6szlyyt66aiu6ecefhr7myrn32w2c5prxng5mq3wu7q"
 }
