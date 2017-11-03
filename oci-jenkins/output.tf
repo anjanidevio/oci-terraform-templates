700645d9a9eb865a907e2c41e67a0f16f87bcedb
yougandar <ynaik@sysgain.com>
2017-11-03 12:00:35 +0530

    Delete output.tf

--- oci-jenkins/output.tf
+++
@@ -1,7 +0,0 @@
-output "linux-a_InstancePublicIP" {
-  value = ["${oci_core_instance.JenkinsInstance.public_ip}"]
-}
-
-output "linux-a_InstancePrivateIP" {
-  value = ["${oci_core_instance.JenkinsInstance.private_ip}"]
-}
