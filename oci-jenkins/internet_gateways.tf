c99c8e321dd4361cda91da8dc21732e2db3ff8c5
yougandar <ynaik@sysgain.com>
2017-11-03 12:00:26 +0530

    Delete internet_gateways.tf

--- oci-jenkins/internet_gateways.tf
+++
@@ -1,5 +0,0 @@
-resource "oci_core_internet_gateway" "internetgateway" {
-    compartment_id = "${var.compartment_ocid}"
-    display_name = "jenkinsgateway"
-    vcn_id = "${oci_core_virtual_network.vcn.id}"
-}
