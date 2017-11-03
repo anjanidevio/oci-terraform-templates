c32cf7b50a5dc1eba890a2e851e3a679a5152f7b
yougandar <ynaik@sysgain.com>
2017-11-03 11:59:00 +0530

    Update jenkin-installscript.sh

--- oci-jenkins/userdata/jenkin-installscript.sh
+++ oci-jenkins/userdata/jenkin-installscript.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 sudo apt-get update
+sudo apt install -y firewalld
+sudo firewall-cmd --list-ports
+sudo firewall-cmd --zone=public --add-port=8080/tcp --permanant
+sudo firewall-cmd --zone=public --add-port=8080/tcp --permanent
+sudo firewall-cmd --reload
+
+#for installing hxselect#
+#sudo apt-get update
+#sudo apt install html-xml-utils
+#sleep 20
+
+#installing jenkins#
+sudo apt-get update
 sudo apt-get install -y default-jre 
-sudo apt-get install -y default-jdk 
-sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get install -y default-jdk
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins
-
-
-apt install -y firewalld
-firewall-cmd --list-ports
-firewall-cmd --zone=public --add-port=8080/tcp --permanant
-firewall-cmd --zone=public --add-port=8080/tcp --permanent
-firewall-cmd --reload
-service jenkins restart
+sudo service jenkins restart
