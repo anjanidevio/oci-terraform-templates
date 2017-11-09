3fa91ba430307a3dff9c1215fa6b84b0db34445c
Komali Oruganti <komalio@Sysgain.com>
2017-11-09 18:46:10 +0530

    up

--- Module_Elktemplate/modules/vcn/vcn.tf
+++ Module_Elktemplate/modules/vcn/vcn.tf
@@ -1,6 +1,6 @@
 resource "oci_core_virtual_network" "ElkVcn" {
   cidr_block = "${var.ElkVcn-CIDR}"
-  dns_label = "${var.VCN-DNSLabel}"
+  dns_label = "${var.DNSLabel}"
   compartment_id = "${var.compartment_id}"
   display_name = "${var.DisplayName}"
 }
