171102a9d40c9e4c2e499834e89926b7cd4147c1
Sushmitha Nalamalpu <Sushmithan@Sysgain.com>
2017-11-09 10:31:25 +0530

    up

---
+++ modules/virtual network/main.tf
@@ -0,0 +1,23 @@
+# Create virtual network
+# The resource names in the module get prefixed by module.<module-instance-name> when instantiated
+
+resource "oci_core_virtual_network" "VCN" {
+  cidr_block = "${var.VCN-CIDR}"
+  dns_label = "${var.VCN-DNSLabel}"
+  compartment_id = "${var.COMP-ID}"
+  display_name = "${var.VCN-DisplayName}"
+}
+
+# Create subnet
+
+#resource "oci_core_subnet" "subnet1" {
+  #compartment_id = "${var.COMP-ID}"
+  #availability_domain = "${lookup(data.oci_identity_availability_domains.availdomain.availability_domains[0],"name")}"
+  #route_table_id      = "${oci_core_route_table.routetable.id}"
+  #vcn_id              = "${oci_core_virtual_network.VCN.id}"
+  #security_list_ids   = ["${oci_core_security_list.securitylist.id}"]
+  #dhcp_options_id     = "${oci_core_dhcp_options.dhcp-options.id}"
+  #dns_label           = "${var.VCN-DisplayName}${random_id.uniqueString.hex}"
+  #display_name        = "${var.VCN-DisplayName}-sub${random_id.uniqueString.hex}"
+  #cidr_block          = "10.0.0.0/24"
+#}
---
+++ modules/virtual network/var.tf
@@ -0,0 +1,15 @@
+variable "VCN-CIDR" {
+  default = "10.0.0.0/16"
+}
+
+variable "VCN-DNSLabel" {
+  default = "splunkvcn"
+}
+
+variable "COMP-ID" {
+  default = "ocid1.compartment.oc1..aaaaaaaajgefvmehc6szlyyt66aiu6ecefhr7myrn32w2c5prxng5mq3wu7q"
+}
+
+variable "VCN-DisplayName" {
+  default = "splunkvcn"
+}
