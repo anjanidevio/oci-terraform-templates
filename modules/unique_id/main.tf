90e01d25347f8bad1d1d7240e51927a4e83c7b6f
Sushmitha Nalamalpu <Sushmithan@Sysgain.com>
2017-11-09 10:37:35 +0530

    del

--- modules/unique_id/main.tf
+++
@@ -1,7 +0,0 @@
-resource "random_id" "uniqueString" {
-  keepers = {
-    #Generate a new id each time we create a VCN environment
-    DataString = "${var.VCN-DisplayName}"
-  }
-  byte_length = 2
-}
