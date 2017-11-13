0b46044ba4ad33adda58007972c6eb28be697eeb
Sushmitha Nalamalpu <Sushmithan@Sysgain.com>
2017-11-13 16:45:45 +0530

    up

--- splunk_enterprise-modules/modules/images_data_source/data.tf
+++ splunk_enterprise-modules/modules/images_data_source/data.tf
@@ -1,5 +1,5 @@
 data "oci_core_images" "ImageOCID" {
-    compartment_ocid = "${var.COMP-ID}"
+    compartment_id = "${var.COMP-ID}"
     operating_system ="${var.InstanceOS}"
     operating_system_version =  "${var.InstanceOSVersion}"
 }
