830cd763481d4865c925e607f56d0af5f36e471d
Komali Oruganti <komalio@Sysgain.com>
2017-11-15 18:11:13 +0530

    up

--- Module_Elktemplate/modules/subnet/var.tf
+++ Module_Elktemplate/modules/subnet/var.tf
@@ -6,5 +6,3 @@ variable "security_list_ocid" {}
 variable "route_table_ocid" {}
 variable "internet_gateway_ocid" {}
 variable "dhcp_ocid" {}
-variable "VCN-DisplayName" {}
-variable "VCN-DNSLabel" {}
