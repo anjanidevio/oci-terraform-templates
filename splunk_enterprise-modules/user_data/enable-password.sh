c4b6e67ba69f95e3ff175de687ef0f37081a9b0a
unknown <srekapalli@sysgain.com>
2017-11-15 17:31:59 +0530

    updated

--- splunk_enterprise-modules/user_data/enable-password.sh
+++ splunk_enterprise-modules/user_data/enable-password.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-username=$1
-pwd=$2
-
-echo -e "ubuntu ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
-echo -e "$username ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
-
+username=ubuntu
+pwd=Password@1234
 ############change username from default username ############
 sudo usermod -l $username ubuntu
 usermod -d /home/$username -m $username
