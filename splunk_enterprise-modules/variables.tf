3ff53415385e105330a7fe0c1f3709ffef21864d
Sushmitha Nalamalpu <Sushmithan@Sysgain.com>
2017-11-16 12:05:32 +0530

    up

--- splunk_enterprise-modules/variables.tf
+++ splunk_enterprise-modules/variables.tf
@@ -1,25 +1,23 @@
-variable "prefix" {
-    default = "splunkvm"
-}
-variable "tenancy_ocid" {
-    type    = "string"
-    default = "ocid1.tenancy.oc1..aaaaaaaaa4jvtf7x54iv54vz77ivqui2nlb7ordznpupnvqbaktl6vkwfhga"
+variable "tenancy_ocid" {  
 }
 
 variable "user_ocid" {
-    type    = "string"
-    default = "ocid1.user.oc1..aaaaaaaat5pk2vhfib7bnf6n3bqsw2mjlieisfv4yfjtw5xvrkwpvvhkiloq"
+    
 }
 
 variable "fingerprint" {
-    type    = "string"
-    default = "71:ea:80:93:a1:92:f8:32:85:05:66:b2:9e:fe:74:40"
+    
 }
 
 variable "private_key_path" {
     type    = "string"
     default = "c:/Users/sushmithan/AppData/Roaming/oci_api_keys/obmcpvt.pem"
 }
+
+variable "prefix" {
+    default = "splunkvm"
+}
+
 
 variable "region" {
     type    = "string"
