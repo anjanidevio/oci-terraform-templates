601252c3d549a80e3fd6d4b7cc44e856c9cef934
Komali Oruganti <komalio@Sysgain.com>
2017-11-16 16:09:26 +0530

    up

--- Module_Elktemplate/output.tf
+++ Module_Elktemplate/output.tf
@@ -2,5 +2,12 @@ output "ELKPublicIP" {
   value = ["${module.instance.ELk-kibanaPublicIP}"]
 }
 output "Clientpublicip" {
-  value = ["${module.instance.ELk-kibanaPublicIP}"]
+  value = ["${module.instance.Clientvmpublicip}"]
+}
+output "adminusername" {
+  value = "${var.admin_username}"
+}
+
+output "adminpassword" {
+  value = "${var.admin_password}"
 }
--- Module_Elktemplate/variables.tf
+++ Module_Elktemplate/variables.tf
@@ -1,25 +1,25 @@
 variable "tenancy_ocid" {
-  default = ""
+  default = "ocid1.tenancy.oc1..aaaaaaaaa4jvtf7x54iv54vz77ivqui2nlb7ordznpupnvqbaktl6vkwfhga"
 }
 
 variable "user_ocid" {
-    default = ""
+    default = "ocid1.user.oc1..aaaaaaaat5pk2vhfib7bnf6n3bqsw2mjlieisfv4yfjtw5xvrkwpvvhkiloq"
 }
 
 variable "fingerprint" {
-    default = ""
+    default = "71:ea:80:93:a1:92:f8:32:85:05:66:b2:9e:fe:74:40"
 }
 
 variable "private_key_url" {
-    default = ""
+    default = "C:/Users/komalio/AppData/Roaming/obmcpvt.pem"
 }
 
 variable "region" {
-    default = ""
+    default = "us-phoenix-1"
 }
 
 variable "compartment_id" {
-    default = ""
+    default = "ocid1.compartment.oc1..aaaaaaaajgefvmehc6szlyyt66aiu6ecefhr7myrn32w2c5prxng5mq3wu7q"
 }
 variable "ElkVcn-CIDR" {
   default = "10.0.0.0/16"
@@ -54,4 +54,10 @@ variable "BootStrapFile1" {
     }
 variable "ssh_private_key" {
     default = "./userdata/elkprivatekey.pem"
+}
+variable "adminusername" {
+    default = "ubuntu"
+}
+variable "adminpassword" {
+    default = "elastic@123"
 }
