caf068359d3d9bed38a45c5ed0e65b3c8c704c6e
komali <komalio@sysgain.com>
2017-11-17 11:15:39 +0530

    Update dhcpoptions.tf

--- Module_Elktemplate/modules/dhcp_options/dhcpoptions.tf
+++ Module_Elktemplate/modules/dhcp_options/dhcpoptions.tf
@@ -1,7 +1,7 @@
 resource "oci_core_dhcp_options" "Elkdhcp-options" {
   compartment_id = "${var.compartment_id}"
   vcn_id = "${var.vcn_ocid}"
-  display_name = "${var.VCN-DisplayName}-dhcpoptions"
+  display_name = "${var.VCN-DisplayName}-dhcp${var.unique_id}"
   options {
     type = "DomainNameServer"
     server_type = "VcnLocalPlusInternet"
