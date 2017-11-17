443e5b525f69a0be956b6fc98e79b0ed785dcff7
Komali Oruganti <komalio@Sysgain.com>
2017-11-17 12:10:20 +0530

    up

--- Module_Elktemplate/modules/vcn/vcn.tf
+++ Module_Elktemplate/modules/vcn/vcn.tf
@@ -1,6 +1,6 @@
 resource "oci_core_virtual_network" "ElkVcn" {
   cidr_block = "${var.ElkVcn-CIDR}"
-  dns_label = "${var.VCN-DNSLabel}-${var.uniqueid}"
+  dns_label = "${var.VCN-DNSLabel}${var.uniqueid}"
   compartment_id = "${var.compartment_id}"
-  display_name = "${var.VCN-DisplayName}-vcn${var.uniqueid}"
+  display_name = "${var.VCN-DisplayName}vcn${var.uniqueid}"
 }
