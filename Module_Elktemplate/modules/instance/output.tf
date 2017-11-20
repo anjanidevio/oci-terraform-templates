08a99773c09ed10d194f93e484404239eb6eb6b0
komali <komalio@sysgain.com>
2017-11-20 11:51:16 +0530

    Update output.tf

--- Module_Elktemplate/modules/instance/output.tf
+++ Module_Elktemplate/modules/instance/output.tf
@@ -7,3 +7,9 @@ output "ELk-kibanaPublicIP" {
 output "Clientvmpublicip" {
   value = ["${oci_core_instance.Clientvm.public_ip}"]
 }
+output "ELk-kibanaPrivateIP" {
+  value = ["${oci_core_instance.Elkvm.private_ip}"]
+}
+output "Clientvmprivateip" {
+  value = ["${oci_core_instance.Clientvm.private_ip}"]
+}
