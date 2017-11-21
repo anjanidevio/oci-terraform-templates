547a60ca0550d594098a9714bc604976746b432c
Komali Oruganti <komalio@Sysgain.com>
2017-11-21 12:55:38 +0530

    up

--- hpe-vertica-clickstream/hpe_blockvolume.tf
+++ hpe-vertica-clickstream/hpe_blockvolume.tf
@@ -1,12 +1,12 @@
 resource "oci_core_volume" "hpevolume" {
   availability_domain = "${lookup(data.oci_identity_availability_domains.AvailDomain.availability_domains[0],"name")}"
-  compartment_id = "${var.compartment_ocid}"
+  compartment_id = "${var.compartment_id}"
   display_name = "hpedatavolume"
-  size_in_gbs = "${var.DBSize}"
+  size_in_mbs = "${var.DBSize}"
 }
 resource "oci_core_volume_attachment" "hpeblockAttach" {
     attachment_type = "iscsi"
-    compartment_id = "${var.compartment_ocid}"
+    compartment_id = "${var.compartment_id}"
     instance_id = "${oci_core_instance.HPEVm.id}"
     volume_id = "${oci_core_volume.hpevolume.id}"
 }
--- hpe-vertica-clickstream/hpe_variables.tf
+++ hpe-vertica-clickstream/hpe_variables.tf
@@ -1,16 +1,17 @@
 variable "tenancy_ocid" {
-
+   default = "ocid1.tenancy.oc1..aaaaaaaaa4jvtf7x54iv54vz77ivqui2nlb7ordznpupnvqbaktl6vkwfhga"
 }
 
 variable "user_ocid" {
-   
+     default = "ocid1.user.oc1..aaaaaaaat5pk2vhfib7bnf6n3bqsw2mjlieisfv4yfjtw5xvrkwpvvhkiloq"
+}
 
 variable "fingerprint" {
-   
+   default ="71:ea:80:93:a1:92:f8:32:85:05:66:b2:9e:fe:74:40"
 }
 
 variable "private_key_url" {
-    default = "/root/.oci/oci_api_key1.pem"
+    default = "C:/Users/komalio/AppData/Roaming/obmcpvt.pem"
 }
 
 variable "region" {
@@ -36,11 +37,11 @@ variable "ssh_public_key" {
     default = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAt6uRj/htFEsnALv9WUAEpIIDOt2+RMeis3x/PWWDaNAO0PAU5GSrXOGbELPqA9Us3tq6nMUXN5XjqYHKqq4Hm7SxJrC95DagDCvD/RKiqq3H3b/oz7CQrtp99xn/sfibPKrUufrnaXW8rb9U71+FPVP3RoIebMNnyA1Fxs2nOR3sbTG7R824SRbmJs2XhLJyU3JEVvLl7SZNfZ10ilswrPa4qmd5H5ixFPij2ocsEQX6Q/thjI7e2OU557jvUkG20L5Bc3XInRvOEPW0i06N8vbWYiQGXl5LdKbXjb5QHz3vpbUCPF11n583LyHU3LzNL4IMSinsLuysQb4KeGFhFQ== rsa-key-20171108"
 }
 variable "InstanceOS" {
-    default = "Canonical Ubuntu"
+    default = "CentOS"
 }
 
 variable "InstanceOSVersion" {
-    default = "16.04"
+    default = "6.9"
 }
 variable "InstanceShape" {
     default = "VM.Standard1.8"
@@ -53,7 +54,7 @@ variable "BootStrapFile" {
 }
 variable "ssh_private_key" {
 
-    default = "./userdata/hpe_privatekey.pem"
+    default = "./userdata/hpe_private.pem"
 
 }
  variable "admin_username" {
