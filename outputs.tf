3f4dca9f8fc086c6c84e52c30a710851cbf9d9ec
unknown <srekapalli@sysgain.com>
2017-11-30 14:53:56 +0530

    update

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,11 @@
-output "dostgrePassword" {
+output "postgrePassword" {
   value = ["${var.postgrePassword}"]
+}
+output "dtsun_InstancePublicIP" {
+  value = ["${module.vnic_data_source.linux_public_ip}"]
 }
 output "dataSunriseGUIUsername" {
   value = ["admin"]
 }
 output "dataSunriseGUIPassword" {
   value = ["${var.dataSunrise-GUI-Password}"]
-}
-
-output "dtsun_InstancePublicIP" {
-  value = ["${module.vnic_data_source.linux_public_ip}"]
-}
