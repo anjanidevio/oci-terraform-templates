c467ad7fccb8436f0647112478a0f207dadb1879
unknown <srekapalli@sysgain.com>
2017-12-01 16:43:24 +0530

    update

--- datasunrise-modules/outputs.tf
+++ datasunrise-modules/outputs.tf
@@ -1,12 +1,12 @@
-output "postgrePassword" {
-  value = "${var.postgrePassword}"
-}
-output "dtsun_InstancePublicIP" {
-  value = "${module.vnic_data_source.linux_public_ip}"
+output "dataSunriseGUIPassword" {
+  value = "${var.dataSunrise-GUI-Password}"
 }
 output "dataSunriseGUIUsername" {
   value = "admin"
 }
-output "dataSunriseGUIPassword" {
-  value = "${var.dataSunrise-GUI-Password}"
+output "dataSunrisePublicIP" {
+  value = "${module.vnic_data_source.linux_public_ip}"
+}
+output "postgrePassword" {
+  value = "${var.postgrePassword}"
 }
