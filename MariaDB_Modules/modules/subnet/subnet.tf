28cd63a458ac7169479c347a9c18d8666335a773
komali <komalio@sysgain.com>
2017-12-05 15:17:46 +0530

    Update subnet.tf

--- MariaDB_Modules/modules/subnet/subnet.tf
+++ MariaDB_Modules/modules/subnet/subnet.tf
@@ -6,7 +6,7 @@ resource "oci_core_subnet" "Mariadbsubnet" {
   vcn_id              = "${var.vcn_ocid}"
   security_list_ids   = ["${var.security_list_ocid}"]
   dhcp_options_id     = "${var.dhcp_ocid}"
-  dns_label           = "mariadbsubnet${var.uniqueid}"
+  dns_label           = "mdbsub${var.uniqueid}"
   display_name        = "${var.VCN-DisplayName}-public${var.uniqueid}"
   cidr_block          = "10.0.0.0/24"
 }
