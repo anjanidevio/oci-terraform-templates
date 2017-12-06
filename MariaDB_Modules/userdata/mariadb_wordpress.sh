83c99e506750009d4fd0657bd47ee49860c1d7b2
komali <komalio@sysgain.com>
2017-12-06 18:31:58 +0530

    Update mariadb_wordpress.sh

--- MariaDB_Modules/userdata/mariadb_wordpress.sh
+++ MariaDB_Modules/userdata/mariadb_wordpress.sh
@@ -4,4 +4,3 @@ sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F2
 sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mirror.jmu.edu/pub/mariadb/repo/10.1/ubuntu xenial main'
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -y mariadb-server
-sudo systemctl start mariadb.service
