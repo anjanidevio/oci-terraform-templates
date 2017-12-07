ff45460dc4ed2a12259bcd59494c585676f09d3b
komali <komalio@sysgain.com>
2017-12-07 12:50:13 +0530

    Update mariadb_wordpress.sh

--- MariaDB_Modules/userdata/mariadb_wordpress.sh
+++ MariaDB_Modules/userdata/mariadb_wordpress.sh
@@ -4,3 +4,10 @@ sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F2
 sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mirror.jmu.edu/pub/mariadb/repo/10.1/ubuntu xenial main'
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -y mariadb-server
+
+#install wordpress
+cd /tmp/ && wget http://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz
+sudo cp -R wordpress/* /var/www/html
+sudo rm -rf /var/www/html/index.html
+sudo cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
