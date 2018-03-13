c3c57f326c982166ae07d03d8c88334c779467ec
sudheermareddy <sudheerm@sysgain.com>
2018-03-13 14:45:59 +0530

    Update chefws_setup2.ps1

--- chef-automate-modules/userdata/chefws_setup2.ps1
+++ chef-automate-modules/userdata/chefws_setup2.ps1
@@ -8,7 +8,7 @@ netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" p
 netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" profile=public protocol=tcp localport=443 remoteip=localsubnet new remoteip=any
 winrm quickconfig -q
 chef generate app c:\users\chef-repo
-git clone https://github.com/sysgain/OCI-ChefTD-cookbooks.git c:/users/cookbookstore
+git clone https://github.com/sysgain/ChefAutomateTD_Cookbooks.git c:/users/cookbookstore
 echo c:\Users\chef-repo\.chef\knife.rb | knife configure --server-url https://10.0.0.3/organizations/orguser --validation-client-name orguser-validator --validation-key c:/Users/chef-repo/.chef/orguser-validator.pem --user delivery --repository c:/Users/chef-repo
 echo n | & "C:\Program Files\PuTTY\pscp.exe"  -scp -pw Password@1234 ubuntu@10.0.0.3:/etc/opscode/delivery.pem C:\Users\chef-repo\.chef\delivery.pem
 echo n | & "C:\Program Files\PuTTY\pscp.exe"  -scp -pw Password@1234 ubuntu@10.0.0.3:/etc/opscode/orguser-validator.pem C:\Users\chef-repo\.chef\orguser-validator.pem
