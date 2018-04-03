2d650f4d21929c3f9916053acb2a185b1a97a6bc
PradeepTS <pradeepts@sysgain.com>
2018-04-03 18:02:38 +0530

    Delete vm2_ansible.sh

--- ansible/vm2_ansible.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "******** python installation *********"
-sudo apt-get update
-apt-get install software-properties-common python-software-properties
-sudo apt-get update
-sudo apt-get install python
-exit
