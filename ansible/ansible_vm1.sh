1d416e90cc0efa51262eec47a65bbc4a5521e3cb
PradeepTS <pradeepts@sysgain.com>
2018-04-03 16:36:09 +0530

    Update ansible_vm1.sh

--- ansible/ansible_vm1.sh
+++ ansible/ansible_vm1.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+
 echo "******************************ansible installation***************************************"
+
 sudo apt-get update
+
 sudo apt-get install software-properties-common
+
 sudo apt-add-repository ppa:ansible/ansible
+
 sudo apt-get update
+
 sudo apt-get install ansible
+
 echo "******************************ansible installed********************************************"
+
 echo "*******************ansible version******************"
+
 ansible --version
