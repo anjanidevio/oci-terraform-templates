480a1224acd84a5ce483a6d3dded982e495feeaf
PradeepTS <pradeepts@sysgain.com>
2018-04-03 18:56:04 +0530

    Update vm2_ansible.sh

--- ansible/vm2_ansible.sh
+++ ansible/vm2_ansible.sh
@@ -4,6 +4,5 @@ sudo apt-get update
 sudo apt-get install software-properties-common python-software-properties -y
 sudo apt-get update
 sudo apt-get install python
-echo "******** python installed *************"
-echo "********python version*****************"
 python --version
+exit
