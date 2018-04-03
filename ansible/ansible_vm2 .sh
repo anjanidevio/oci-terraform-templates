00f382edc8e26691a342be02e6042f2a2a030f56
PradeepTS <pradeepts@sysgain.com>
2018-04-03 15:54:35 +0530

    Add files via upload

---
+++ ansible/ansible_vm2 .sh
@@ -0,0 +1,7 @@
+#!bin/bash
+echo "**************************python installation***************************"
+sudo apt-get update
+apt-get install software-properties-common python-software-properties
+sudo apt-get update
+sudo apt-get install python
+echo "**************************python installed******************************"
